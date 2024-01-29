dc40138b8b50a1834106f998986125a4e9a28f32
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-29 22:49:53 +0300

    Custom inputu hariç diğer elemanların JS işlemleri tamamlandı. inputların boş , sayı olmama gibi durumlarına göre gerekli hata mesajları timeout fonksiyonu ile gösterilmesi işlemi yapıldı. her inputta input elemanı dinlenerek içerikte ki fiyatlandırmaların anlık olarak değişmesi işlemi yapıldı.

--- assets/js/tip-calculator.js
+++ assets/js/tip-calculator.js
@@ -6,12 +6,65 @@ const resetBtn = document.querySelector('.reset-btn');
 const tipAmount = document.querySelector('#tip-amount-result');
 const total = document.querySelector('#total-result');
 
+let totalTip = 0;
+let calcPeopleAgain = 0;
+let discountRate = 0;
+
 for (const discount of discountBtns) {
     discount.addEventListener('click', e => selectTip(e));
 }
 
 billInput.addEventListener('input' , e => billInputResult(e))
 
+numberOfPeopleInput.addEventListener('input' , e => numberOfPeople(e))
+
+function numberOfPeople(e){
+    calcPeopleAgain = e.target.value;
+    if(totalTip > 0){
+        total.textContent = `$${((e.target.value / calcPeopleAgain) + totalTip)}`;
+    }else if(billInput.value > 0){
+        total.textContent = `$${billInput.value / calcPeopleAgain}`;
+    }
+}
+
 function billInputResult(e){
-    total.textContent = `$${e.target.value}`;
+    if(!isNaN(e.target.value) && numberOfPeopleInput.value > 0 && calcPeopleAgain > 0){
+        if(totalTip > 0){
+            totalTip = eval(((e.target.value * discountRate)/100)/calcPeopleAgain)
+            total.textContent = `$${((e.target.value / calcPeopleAgain) + totalTip)}`;
+            tipAmount.textContent = totalTip;
+        }else if(calcPeopleAgain > 0){
+            total.textContent = `$${e.target.value / calcPeopleAgain}`;
+        }
+    }
+    return
+}
+
+function selectTip(e){
+    e.preventDefault();
+    
+    if(numberOfPeopleInput.value > 0 && !isNaN(numberOfPeopleInput.value)){
+        for (const discount of discountBtns) {
+            if(e.target == discount){
+                e.target.classList.add('active');
+            }else{
+                discount.classList.remove('active');
+            }
+        }
+        totalTip = ((billInput.value * e.target.dataset.value)/100)/numberOfPeopleInput.value
+        discountRate = e.target.dataset.value;
+        tipAmount.textContent = `$${((billInput.value * e.target.dataset.value)/100)/numberOfPeopleInput.value}`;
+        total.textContent = `$${eval((billInput.value / numberOfPeopleInput.value) + totalTip)}`
+    }else{
+        tipAmount.textContent = "Error";
+        billInput.value = "Error";
+        numberOfPeopleInput.value = "Error";
+        setTimeout(hideError,1000)
+    }
+}
+
+function hideError(){
+    tipAmount.textContent = "$0.00";
+    billInput.value = "";
+    numberOfPeopleInput.value = "";
 }
