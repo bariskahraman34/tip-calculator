b16aace0f0db30b254c37747252b839fda95d959
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-30 15:48:01 +0300

    İşlem fazlalıklarından kurtulmak için calculateTip adında bir fonksiyon oluşturuldu ve her bir inputtan değer alması için içine parametreler verildi. Her bir input değiştiğinde bahşişte beraberinde değişmesi için her bir inputta bu değerlerin tekrar gönderilmesi işlemi yapıldı.

--- assets/js/tip-calculator.js
+++ assets/js/tip-calculator.js
@@ -6,10 +6,11 @@ const resetBtn = document.querySelector('.reset-btn');
 const tipAmount = document.querySelector('#tip-amount-result');
 const total = document.querySelector('#total-result');
 
-
-let totalTip = 0;
-let calcPeopleAgain = 0;
+let bill = 0;
+let numOfPeople = 0;
 let discountRate = 0;
+let totalTipPerPerson = 0;
+let totalPerPerson = 0;
 
 for (const discount of discountBtns) {
     discount.addEventListener('click', e => selectTip(e));
@@ -24,7 +25,7 @@ numberOfPeopleInput.addEventListener('input' , e => numberOfPeople(e))
 resetBtn.addEventListener('click',resetBoard)
 
 function handleResetBtn(){
-    if(total.textContent != "$0.00" && total.textContent != "Error"){
+    if(total.textContent != "$0.00"){
         resetBtn.classList.add('active');
         resetBtn.classList.remove('not-active');
     }else{
@@ -34,73 +35,66 @@ function handleResetBtn(){
 }
 
 function numberOfPeople(e){
-    calcPeopleAgain = e.target.value;
-    totalTip = ((billInput.value * discountRate)/100)
-    if(e.target.value === "0"){
-        document.querySelectorAll('.input-container')[1].style.border = "2px solid #E17052";
-        let notZero = `<span class="zero">Can't be zero</span>`
-        document.querySelector('.number-of-people-container h3').insertAdjacentHTML('afterend',notZero) 
-        return
-    }else{
-        document.querySelectorAll('.input-container')[1].style.border = "2px solid #26C2AE";
-        document.querySelector('.zero').innerHTML = "";
-        
+    numOfPeople = e.target.value;
+    // if(numOfPeople === "0"){
+    //     document.querySelectorAll('.input-container')[1].style.border = "2px solid #E17052";
+    //     let notZero = `<span class="zero">Can't be zero</span>`
+    //     document.querySelector('.number-of-people-container h3').insertAdjacentHTML('afterend',notZero) 
+    //     return
+    // }else{
+    //     document.querySelectorAll('.input-container')[1].style.border = "2px solid #26C2AE";
+    //     document.querySelector('.zero').innerHTML = "";
+    // }
+    if(numOfPeople > 0 && bill > 0 && discountRate > 0){
+        calculateTip(bill,numOfPeople,discountRate);
     }
-    if(totalTip > 0){
-        total.textContent = `$${((billInput.value / calcPeopleAgain) + totalTip/calcPeopleAgain)}`;
-        tipAmount.textContent = `$${totalTip/calcPeopleAgain}`;
-    }else if(billInput.value > 0 && totalTip <= 0){
-        total.textContent = `$${billInput.value / calcPeopleAgain}`;
-        tipAmount.textContent = `$0.00`;
-    }
-    if(calcPeopleAgain == ""){
-        total.textContent = "$0.00"
-        tipAmount.textContent = "$0.00"
-    }
-    handleResetBtn()
 }
 
 function billInputResult(e){
-    if(!isNaN(e.target.value) && numberOfPeopleInput.value > 0 && calcPeopleAgain > 0){
-        if(totalTip > 0){
-            totalTip = eval(((e.target.value * discountRate)/100)/calcPeopleAgain)
-            total.textContent = `$${((e.target.value / calcPeopleAgain) + totalTip)}`;
-            tipAmount.textContent = totalTip;
-        }else if(calcPeopleAgain > 0){
-            total.textContent = `$${e.target.value / calcPeopleAgain}`;
-        }
-        handleResetBtn()
+    bill = e.target.value;
+    // if(bill === "0"){
+    //     document.querySelectorAll('.input-container')[0].style.border = "2px solid #E17052";
+    //     let notZero = `<span class="zero">Can't be zero</span>`
+    //     document.querySelector('.number-of-people-container h3').insertAdjacentHTML('afterend',notZero) 
+    //     return
+    // }else{
+    //     document.querySelectorAll('.input-container')[0].style.border = "2px solid #26C2AE";
+    //     document.querySelector('.zero').innerHTML = "";
+    // }
+    if(numOfPeople > 0 && bill > 0 && discountRate > 0){
+        calculateTip(bill,numOfPeople,discountRate);
     }
-    return
 }
 
 function selectTip(e){
     e.preventDefault();
-    if(numberOfPeopleInput.value > 0 && !isNaN(numberOfPeopleInput.value)){
-        for (const discount of discountBtns) {
-            if(e.target == discount){
-                e.target.classList.add('active');
-            }else{
-                discount.classList.remove('active');
-            }
+    for (const discount of discountBtns) {
+        if(e.target == discount){
+            e.target.classList.add('active');
+        }else{
+            discount.classList.remove('active');
         }
-        discountRate = e.target.dataset.value;
-        totalTip = (((billInput.value * discountRate)/100)/numberOfPeopleInput.value)
-        tipAmount.textContent = `$${(((billInput.value * discountRate)/100)/numberOfPeopleInput.value)}`;
-        total.textContent = `$${((billInput.value / numberOfPeopleInput.value) + totalTip)}`
-    }else{
-        tipAmount.textContent = "Error";
-        billInput.value = "Error";
-        numberOfPeopleInput.value = "Error";
-        setTimeout(hideError,1000)
     }
-    handleResetBtn()
+    discountRate = e.target.dataset.value;
+    if(numOfPeople > 0 && bill > 0 && discountRate > 0){
+        calculateTip(bill,numOfPeople,discountRate);
+    }
 }
 
-function hideError(){
-    tipAmount.textContent = "$0.00";
-    billInput.value = "";
-    numberOfPeopleInput.value = "";
+function addCustomDiscount(e){
+    if(e.target.value != ""){
+        for (const discount of discountBtns) {
+            discount.classList.remove('active');
+        }
+        discountRate = e.target.value;
+        if(numOfPeople > 0 && bill > 0 && discountRate > 0){
+            calculateTip(bill,numOfPeople,discountRate);
+        }
+    }
+}
+
+function calculateTip(bill,numOfPeople,discountRate){
+    console.log(bill,numOfPeople,discountRate);
 }
 
 function resetBoard(){
@@ -116,22 +110,3 @@ function resetBoard(){
     resetBtn.classList.add('not-active');
 }
 
-function addCustomDiscount(e){
-    if(e.target.value != ""){
-        for (const discount of discountBtns) {
-            discount.classList.remove('active');
-        }
-        discountRate = e.target.value;
-        if(numberOfPeopleInput.value > 0 && !isNaN(numberOfPeopleInput.value)){
-            totalTip = (((billInput.value * discountRate)/100)/numberOfPeopleInput.value)
-            tipAmount.textContent = `$${(((billInput.value * discountRate)/100)/numberOfPeopleInput.value)}`;
-            total.textContent = `$${eval((billInput.value / numberOfPeopleInput.value) + totalTip)}`
-        }else{
-            tipAmount.textContent = "Error";
-            billInput.value = "Error";
-            numberOfPeopleInput.value = "Error";
-            setTimeout(hideError,1000)
-        }
-        handleResetBtn();
-    }
-}
